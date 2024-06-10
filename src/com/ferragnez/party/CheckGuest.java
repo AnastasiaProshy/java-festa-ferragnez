8e822992b2e92acc5a4bfb76967bc587883977f4
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-06-10 20:20:00 +0200

    added: project sketch
    package & class
    scanner import
    git-ignore file

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,11 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner noteDown = new Scanner(System.in); //scanner assignment
+		
+}
